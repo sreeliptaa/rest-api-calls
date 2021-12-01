039a53bf1757f096fe68f9ae136c99b17c50b1a6
sreeliptaa <sreelipta65@gmail.com>
2021-12-01 21:40:47 +0530

    Added : Project files from spring initialiser

---
+++ src/main/java/com/bridgelabz/RestApiCallsApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RestApiCallsApplication.class, args);
+	}
+
+}
