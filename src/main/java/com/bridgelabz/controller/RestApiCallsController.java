68b9d2a48a7cea2c1f78b4443616be0869f3d610
sreeliptaa <sreelipta65@gmail.com>
2021-12-01 22:49:32 +0530

    Added : Get request method and passing name as path variable to display message in RestApiCallsController class

--- src/main/java/com/bridgelabz/controller/RestApiCallsController.java
+++ src/main/java/com/bridgelabz/controller/RestApiCallsController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class RestApiCallsController {
     @GetMapping(value = "/hello2")
     public String getMessage(@RequestParam String name) {
         return " Hello " + name + " From Bridgelabz ";
+    }
+
+    @GetMapping(value = "/hello3/{name}")
+    public String getMessageUsingPathVariable
+            (@PathVariable String name) {
+        return " Hello " + name + " From BridgeLabz ";
     }
 }
